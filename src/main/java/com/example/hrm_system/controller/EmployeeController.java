5c01e47024b3a9c1fd30f431d4ce49e985fab3f8
MarimEl-akhal <marimelakhel94@gmail.com>
2026-02-09 16:44:20 +0200

    test: add test get employee info should return noy found when not found employee and refactor: rewrite controller method

--- src/main/java/com/example/hrm_system/controller/EmployeeController.java
+++ src/main/java/com/example/hrm_system/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.hrm_system.controller;
 import com.example.hrm_system.dto.EmployeeResponse;
 import com.example.hrm_system.entity.Employee;
 import com.example.hrm_system.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,9 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public EmployeeResponse findEmployeeById(@PathVariable Long id) {
-        return employeeService.findById(id);
+    public ResponseEntity<EmployeeResponse> findEmployeeById(@PathVariable Long id) {
+        EmployeeResponse responseDto = employeeService.findById(id);
+        return ResponseEntity.ok(responseDto);
     }
 
 
--- src/test/java/com/example/hrm_system/integration/EmployeeControllerTest.java
+++ src/test/java/com/example/hrm_system/integration/EmployeeControllerTest.java
@@ -4,29 +4,39 @@ import com.example.hrm_system.dto.EmployeeResponse;
 import com.example.hrm_system.entity.Employee;
 import com.example.hrm_system.enums.Gender;
 import com.example.hrm_system.repository.EmployeeRepository;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static com.example.hrm_system.enums.ApiError.EMPLOYEE_NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+@DbUnitConfiguration(databaseConnection = "dataSource")
 @TestPropertySource(locations = "classpath:application-test.yml")
 public class EmployeeControllerTest {
     @Autowired
@@ -39,8 +49,8 @@ public class EmployeeControllerTest {
     private EmployeeRepository employeeRepository;
 
     @Test
-    @Transactional
     @DatabaseSetup("/dataset/get-employee-info.xml")
+    @Transactional
     void testGetEmployeeInfo_shouldReturnOkWhenFindEmployeeById() throws Exception {
         MvcResult result = mockMvc.perform(get("/api/employees/1"))
                 .andExpect(status().isOk())
@@ -48,7 +58,7 @@ public class EmployeeControllerTest {
         EmployeeResponse employeeResponse = objectMapper
                 .readValue(result.getResponse().getContentAsString(), EmployeeResponse.class);
 
-      Employee employee = employeeRepository.findById(employeeResponse.getId()).get();
+        Employee employee = employeeRepository.findById(employeeResponse.getId()).get();
 
         assertNotNull(employee);
         assertNotNull(employee.getId());
@@ -57,7 +67,17 @@ public class EmployeeControllerTest {
         assertEquals(LocalDate.of(1975, 1, 1), employee.getBirthDate());
         assertEquals(LocalDate.of(2000, 1, 1), employee.getGraduationDate());
         assertEquals(Gender.FEMALE, employee.getGender());
-        assertEquals(60000.0, employee.getGrossSalary());
+        assertEquals(100000.0, employee.getGrossSalary());
+    }
+
+    @Test
+    @Transactional
+    @DatabaseSetup("/dataset/get-employee-info.xml")
+    void testGetEmployeeInfo_shouldReturnNotFoundWhenEmployeeNotFound() throws Exception {
+        mockMvc.perform(get("/api/employees/99"))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResponse().getContentAsString()
+                                                    .contains(EMPLOYEE_NOT_FOUND.getDefaultMessage())));
     }
 
 
