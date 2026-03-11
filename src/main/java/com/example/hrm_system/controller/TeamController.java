a036e5f1fff1653eb8070991bbe129c39ce1ff1a
MarimEl-akhal <marimelakhel94@gmail.com>
2026-03-11 10:28:33 +0200

    refactor(team): update controller to use PagingResult for paginated responses with sort

--- src/main/java/com/example/hrm_system/controller/TeamController.java
+++ src/main/java/com/example/hrm_system/controller/TeamController.java
@@ -5,6 +5,7 @@ import com.example.hrm_system.dto.PagingResult;
 import com.example.hrm_system.service.TeamService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +21,13 @@ public class TeamController {
     public ResponseEntity<PagingResult<EmployeeResponse>> getAllEmployeesByTeamId(
             @PathVariable Long teamId,
             @RequestParam(required = false, defaultValue = "0") int pageNo,
-            @RequestParam(required = false, defaultValue = "5") int pageSize
+            @RequestParam(required = false, defaultValue = "5") int pageSize,
+            @RequestParam(required = false, defaultValue = "id") String sortField,
+            @RequestParam(required = false, defaultValue = "ASC") Sort.Direction direction
     ) {
 
         PagingResult<EmployeeResponse> responses =
-                teamService.getAllEmployeesByTeamId(teamId, PageRequest.of(pageNo, pageSize));
+                teamService.getAllEmployeesByTeamId(teamId, PageRequest.of(pageNo, pageSize, Sort.by(direction, sortField)));
 
         return ResponseEntity.status(HttpStatus.OK).body(responses);
     }
